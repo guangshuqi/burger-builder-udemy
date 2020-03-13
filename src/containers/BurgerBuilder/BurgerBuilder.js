5b7d208fb057f6d29847ccd8175c812a4510008e
John Qi <guangshuqi@gmail.com>
2020-03-12 22:11:00 -0400

    finished lecture 159

---
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+import Aux from "../../hoc/Auxiliary";
+import Burger from "../../components/Burger/Burger";
+
+class BurgerBuilder extends Component {
+  render() {
+    return (
+      <Aux>
+        <div>
+          <Burger />
+        </div>
+        <div>Build Controls</div>
+      </Aux>
+    );
+  }
+}
+
+export default BurgerBuilder;
